5b99c0645af8a22e0600d1a5dd0d2d7aefaf799c
Howard <howardn@egb070000007.lancs.local>
2019-11-26 15:53:31 +0000

    Creating directory entry per row in Directory file

--- src/Directory.java
+++ src/Directory.java
@@ -4,11 +4,25 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class Directory {
-    public Directory(int pointer, RandomAccessFile file) throws IOException {
+
+
+    public Directory(int pointer, int directoryLength, RandomAccessFile file) throws IOException {
 
         ByteBuffer buffer = Helper.wrap(1024, file, pointer * 1024);
+
+        int ptr = 0;
+        // loop buffer until at end of directory
+        //use .position to seek into buffer by pointer
+        // increment pointer by length value
+        //create data entry from buffer
+
+        while (ptr < size) {
+
+        }
+
         //System.out.println(Arrays.toString(buffer.array()));
         Helper.dumpHexBytes(buffer.array());
         //System.out.println(Arrays.toString(buffer.array()));
     }
+
 }
---
+++ src/DirectoryEntry.java
@@ -0,0 +1,13 @@
+public class DirectoryEntry {
+
+    private Inode inode;
+    private short length;
+    private byte nameLen;
+    private short fileType;
+    private byte[] fileName;
+
+    public DirectoryEntry() {
+
+    }
+
+}
